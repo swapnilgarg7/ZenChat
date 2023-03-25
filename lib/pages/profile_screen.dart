544e7a0f23f23e0af3028864b55c4691e589039d
Shivam Mittal <89904835+CodeGeek04@users.noreply.github.com>
2023-03-25 11:09:45 +0530

    Update profile_screen.dart

--- lib/pages/profile_screen.dart
+++ lib/pages/profile_screen.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'subs.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -8,12 +11,29 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  void subscription(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return show_subscription();
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: const Center(
-        child: Text('Profile Screen'),
-      ),
-    );
+        child: Column(
+      children: [
+        Text('Profile Screen'),
+        Image(image: image, width: 75, height: 75),
+        Text('Name'),
+        Text('Gender'),
+        Text('Email'),
+        ElevatedButton(
+          child: Text('SHOW SUBSCRIPTIONS'),
+          onPressed: () => show_subscription(),
+        ),
+      ],
+    ));
   }
 }
