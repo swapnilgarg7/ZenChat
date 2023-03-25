b6ea34aab8809d032e7bb3c8507f9d176df3cf42
CodeGeek04 <shivammittal2124@gmail.com>
2023-03-25 23:19:02 +0530

    integrated model

---
+++ lib/pages/chat/key.dart
@@ -0,0 +1,4 @@
+class key {
+  static String openai_key =
+      "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";
+}
--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'chat/chat_api.dart';
+import 'chat/chat_page.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,8 +14,26 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Text('Home Screen'),
+    return Container(child: ChatPage(chatApi: ChatApi()));
+  }
+}
+
+class ChatApp extends StatelessWidget {
+  const ChatApp({required this.chatApi, super.key});
+
+  final ChatApi chatApi;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ChatGPT Client',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.teal.shade50,
+          secondary: Colors.white,
+        ),
+      ),
+      home: ChatPage(chatApi: chatApi),
     );
   }
 }
