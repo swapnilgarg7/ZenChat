34f1cd035d2f3380b3e1d36d8c902954f22c7792
Mansi joshi <joshimansi32@gmail.com>
2023-03-25 20:31:23 +0530

    profile and subscription UI

--- lib/Helper/sharedPreference.dart
+++ lib/Helper/sharedPreference.dart
@@ -27,4 +27,14 @@ class SharedPreferenceFucntion {
     SharedPreferences sf = await SharedPreferences.getInstance();
     return sf.getBool(userLoggedInKey);
   }
+
+  static Future<String?> getUserEmailFromSF() async {
+    SharedPreferences sf = await SharedPreferences.getInstance();
+    return sf.getString(userEmailKey);
+  }
+
+  static Future<String?> getUserNameFromSF() async {
+    SharedPreferences sf = await SharedPreferences.getInstance();
+    return sf.getString(userNameKey);
+  }
 }
--- lib/pages/subs.dart
+++ lib/pages/subs.dart
@@ -1,57 +1,170 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class show_subscription extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Text('CHOOSE YOUR PLAN'),
-        SizedBox(
-          height: 10,
-          width: double.infinity,
-        ),
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text('400/WEEK'),
-            ElevatedButton(
-              child: Text('SUBSCRIBE'),
-              onPressed: () {},
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          const SizedBox(height: 25),
+          Text(
+            "CHOOSE YOUR PLAN",
+            style: GoogleFonts.merriweather(
+              textStyle: TextStyle(
+                fontSize: MediaQuery.of(context).size.width * 0.07,
+                fontWeight: FontWeight.bold,
+                color: Colors.black87,
+              ),
             ),
-          ],
-        ),
-        SizedBox(
-          height: 10,
-          width: double.infinity,
-        ),
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text('1500/MONTH'),
-            ElevatedButton(
-              child: Text('SUBSCRIBE'),
-              onPressed: () {},
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(
+            height: 20,
+            width: double.infinity,
+          ),
+          Container(
+            color: Theme.of(context).primaryColor,
+            child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: Text(
+                      '400 INR/WEEK',
+                      style: TextStyle(
+                        fontSize: MediaQuery.of(context).size.width * 0.05,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 200,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black,
+                        elevation: 0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        "Subscribe",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
-        SizedBox(
-          height: 10,
-          width: double.infinity,
-        ),
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text('5000/QUARTER'),
-            ElevatedButton(
-              child: Text('SUBSCRIBE'),
-              onPressed: () {},
+          ),
+          SizedBox(
+            height: 10,
+            width: double.infinity,
+          ),
+          Container(
+            color: Theme.of(context).primaryColor,
+            child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: Text(
+                      '1500 INR/MONTH',
+                      style: TextStyle(
+                        fontSize: MediaQuery.of(context).size.width * 0.05,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 200,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black,
+                        elevation: 0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        "Subscribe",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                  ),
+                ],
+              ),
             ),
-          ],
-        )
-      ],
+          ),
+          SizedBox(
+            height: 10,
+            width: double.infinity,
+          ),
+          Container(
+            color: Theme.of(context).primaryColor,
+            child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: Text(
+                      '5000 INR/QUARTER',
+                      style: TextStyle(
+                        fontSize: MediaQuery.of(context).size.width * 0.05,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 200,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black,
+                        elevation: 0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        "Subscribe",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
