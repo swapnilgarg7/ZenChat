8a080293e459a56a2ea0f28d64e483e46efc847a
swapnilgarg7 <swapnilgarg810@gmail.com>
2023-03-25 16:25:00 +0530

    profile screen frontend

--- lib/pages/profile_screen.dart
+++ lib/pages/profile_screen.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'subs.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -24,12 +25,38 @@ class _ProfileScreenState extends State<ProfileScreen> {
     return Container(
         child: Column(
       children: [
-        Text('Profile Screen'),
-        // Image(image: image, width: 75, height: 75),
-        Text('Name'),
-        Text('Gender'),
-        Text('Email'),
+        Lottie.network(
+            'https://assets3.lottiefiles.com/packages/lf20_WKdnG2.json',
+            repeat: false),
+        Text(
+          "Name : ",
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 10),
+        Text(
+          "Gender : ",
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 10),
+        Text(
+          "Email : ",
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 30),
         ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            primary: Colors.teal[700],
+            onPrimary: Colors.white,
+          ),
           child: Text('SHOW SUBSCRIPTIONS'),
           onPressed: () => subscription(context),
         ),
