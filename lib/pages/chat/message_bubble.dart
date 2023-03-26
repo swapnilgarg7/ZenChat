4597da49dc9513d08ddb10563810b385deb1c6c3
CodeGeek04 <shivammittal2124@gmail.com>
2023-03-26 15:50:45 +0530

    Message bubble orientation changed

--- lib/pages/chat/message_bubble.dart
+++ lib/pages/chat/message_bubble.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:zenchat/Helper/sharedPreference.dart';
 
-class MessageBubble extends StatelessWidget {
+class MessageBubble extends StatefulWidget {
   const MessageBubble({
     required this.content,
     required this.isUserMessage,
@@ -11,34 +12,72 @@ class MessageBubble extends StatelessWidget {
   final bool isUserMessage;
 
   @override
+  State<MessageBubble> createState() => _MessageBubbleState();
+}
+
+class _MessageBubbleState extends State<MessageBubble> {
+  String name = 'Anonymous';
+
+  void initState() {
+    getName();
+  }
+
+  Future<void> getName() async {
+    await SharedPreferenceFucntion.getUserNameFromSF().then((value) {
+      if (value == null) {
+        return;
+      }
+      setState(() {
+        name = value;
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     final themeData = Theme.of(context);
-    return Container(
-      margin: const EdgeInsets.all(8),
-      decoration: BoxDecoration(
-        color: isUserMessage
-            ? themeData.colorScheme.primary.withOpacity(0.4)
-            : Colors.teal[700]?.withOpacity(0.4),
-        borderRadius: const BorderRadius.all(Radius.circular(12)),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(12),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                Text(
-                  isUserMessage ? 'You' : 'ZenChat',
-                  style: const TextStyle(fontWeight: FontWeight.bold),
-                ),
-              ],
+    return Flex(
+        direction: Axis.horizontal,
+        mainAxisAlignment: widget.isUserMessage
+            ? MainAxisAlignment.end
+            : MainAxisAlignment.start,
+        children: [
+          Container(
+            constraints: BoxConstraints(
+              maxWidth: MediaQuery.of(context).size.width * 0.7,
             ),
-            const SizedBox(height: 8),
-            Text(content),
-          ],
-        ),
-      ),
-    );
+            margin: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: widget.isUserMessage
+                  ? themeData.colorScheme.primary.withOpacity(0.4)
+                  : Colors.teal[700]?.withOpacity(0.4),
+              borderRadius: const BorderRadius.all(Radius.circular(12)),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(12),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Align(
+                    alignment: widget.isUserMessage
+                        ? Alignment.topLeft
+                        : Alignment.topLeft,
+                    child: Text(
+                      widget.isUserMessage ? '$name' : 'ZenChat',
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Align(
+                    alignment: widget.isUserMessage
+                        ? Alignment.topLeft
+                        : Alignment.topLeft,
+                    child: Text(widget.content),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ]);
   }
 }
